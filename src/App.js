36d84e5d1c6505798f321418f9a54954be3f7ac8
Sean McCarron <sean.mccarron@postcodelottery.co.uk>
2020-09-26 18:24:37 +0100

    changed the state of name value by use of setState method on click of a button

--- src/App.js
+++ src/App.js
@@ -11,12 +11,24 @@ class App extends Component {
     ]
   }
 
+  swtichNameHandler = () => {
+    // console.log('Was clicked!');
+    // DONT DO THIS: this.state.persons[0].name = 'Maximillian';
+    this.setState({
+      persons: [
+        { name: 'Maximillian', age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 26 }
+      ]
+    } )
+  }
+
   render() {
     return (
       <div className="App">
       <h1>Hi, I'm a React App</h1>
       <p>This is really working!</p>
-      <button>Switch Name</button>
+      <button onClick={this.swtichNameHandler}>Switch Name</button>
       <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
       <Person name={this.state.persons[1].name} age={this.state.persons[1].age} > My Hobbies: Racing</Person>
       <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
